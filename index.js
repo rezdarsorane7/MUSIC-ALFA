ed4d9e9bedf03f0f88cda10a93dd7b91e3f6da78
alfa11033 <67869229+rezdarsorane7@users.noreply.github.com>
2021-07-18 06:39:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,37 +5,3 @@ client.build();
 
 module.exports = client; //;-;
 
-client.on("guildCreate", guild => {
-  let channel = client.channels.cache.get("866158078008360970");
-  let embed = new MessageEmbed().setColor("#ee0202")
-  .setAuthor(client.user.username, client.user.avatarURL())
-  .setTitle( ✅ Join Server)
-  .addField(" Server Name", `${guild.name}`)
-  .addField(" Server Owner", `${guild.owner}`)
-  .addField(" Server Id", `${guild.id}`)
-  .addField(" Member Count", `${guild.memberCount}`)
-  .setFooter(`${client.user.tag}`);
-  channel.send(embed);
-});
-
-client.on("guildDelete", guild => {
-  let channel = client.channels.cache.get("866158078008360970");
-  let embed = new MessageEmbed()
-  .setColor("#ee0202")
-  .setAuthor(client.user.username, client.user.avatarURL())
-  .setTitle( ❌ Left Server)
-  .addField(" Server Name", `${guild.name}`)
-  .addField(" Server Owner", `${guild.owner}`)
-  .addField(" Server Id", `${guild.id}`)
-  .addField(" Member Count", `${guild.memberCount}`)
-  .setFooter(`${client.user.tag}`);
-  channel.send(embed);
-});
-
-function delay(delayInms) {
- return new Promise(resolve => {
-   setTimeout(() => {
-     resolve(2);
-   }, delayInms);
- });
-}
